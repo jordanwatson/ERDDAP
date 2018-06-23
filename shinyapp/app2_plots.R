436a2d23cb735708b3d1859b24535cfa4561aa55
Jordan Watson <jordanwatson@users.noreply.github.com>
2018-06-23 09:46:45 -0800

    Multi-tab layout

--- shinyapp/app2_plots.R
+++ shinyapp/app2_plots.R
@@ -7,10 +7,13 @@ data <- readRDS("Data/mur_SST_stat6_all_columns.rds")
 data <- data %>% mutate(newyr=ifelse(month<4,year-1,year),
                         month2=ifelse(month<4,month+12,month),
                         season=ifelse(month2%in%c(4:9),"Summer (Apr - Sept)","Winter (Oct - Mar)"),
-                        GOA=ifelse(is.na(GOA),0,GOA)) 
+                        GOA=ifelse(is.na(GOA),0,GOA),
+                        cumwk=(year-2003)*52+week,
+                        cummo=(year-2003)*12+month) 
          
 ui <- fluidPage(
-  titlePanel("Stat area"),
+  titlePanel("Daily temperatures by ADFG statistical area"),
+  p("Select ADFG statistical area to plot a daily temperature time series. Select multiple areas to compare time series."),
   fluidRow(
     column(4,
              selectInput("STAT_AREA", 
@@ -37,13 +40,19 @@ server <- shinyServer(function(input, output) {
     
     output$mainplot <- renderPlot({
       #dataset <- rxData()  # this is the subsetted data
-      p <- ggplot(rxData(), aes(x = date, y = sst.mean,color=factor(STAT_AREA)))
+      p <- ggplot(rxData() %>% mutate(date=as.POSIXct(date)), aes(x = date, y = sst.mean,color=factor(STAT_AREA)))
       p <- p + 
         geom_line() + 
         theme_bw() + 
         xlab("Date") + 
         ylab("Temperature") + 
-        scale_color_viridis(discrete=TRUE)
+        scale_color_viridis(discrete=TRUE,guide=guide_legend("Statistical Area")) + 
+        scale_x_datetime(date_breaks="1 year",date_labels="%Y") + 
+        theme(legend.position="top",
+              legend.text=element_text(size=14),
+              legend.title=element_text(size=14),
+              axis.text = element_text(size=13),
+              axis.title = element_text(size=14))
       print(p)
     })
   })
@@ -51,38 +60,127 @@ server <- shinyServer(function(input, output) {
 shinyApp(ui, server)
 
 
+#---------------------------------------------
+#  Multi panel
+#---------------------------------------------
 
 
-dataset <- diamonds
 
-shinyUI(fluidPage(
-  
-  title = "Diamonds Explorer",
-  
-  plotOutput('plot'),
-  
-  hr(),
-  
-  fluidRow(
-    column(3,
-           h4("Diamonds Explorer"),
-           sliderInput('sampleSize', 'Sample Size', 
-                       min=1, max=nrow(dataset), value=min(1000, nrow(dataset)), 
-                       step=500, round=0),
-           br(),
-           checkboxInput('jitter', 'Jitter'),
-           checkboxInput('smooth', 'Smooth')
+ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("Daily temperatures",
+             titlePanel("Daily temperatures by ADFG statistical area"),
+             p("Select ADFG statistical area to plot a daily temperature time series. Select multiple areas to compare time series."),
+             fluidRow(
+               column(4,
+                      selectInput("dstat",label = "Choose statistical areas to compare",choices = data$STAT_AREA %>% unique %>% sort,multiple = TRUE)
+               ),
+               plotOutput('daily_plot')
+             )
     ),
-    column(4, offset = 1,
-           selectInput('x', 'X', names(dataset)),
-           selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-           selectInput('color', 'Color', c('None', names(dataset)))
+    tabPanel("Weekly temperatures",
+             titlePanel("Weekly temperatures by ADFG statistical area"),
+             p("Select ADFG statistical area to plot a weekly temperature time series (daily temperatures averaged by week). Select multiple areas to compare time series."),
+             fluidRow(
+               column(4,
+                      selectInput("wstat",label = "Choose statistical areas to compare",choices = data$STAT_AREA %>% unique %>% sort,multiple = TRUE)
+               ),
+               plotOutput('weekly_plot')
+             )
     ),
-    column(4,
-           selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-           selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-    )
-  )
-))
+    tabPanel("Monthly temperatures",
+             titlePanel("Monthly temperatures by ADFG statistical area"),
+             p("Select ADFG statistical area to plot a monthly temperature time series (daily temperatures averaged by month). Select multiple areas to compare time series."),
+             fluidRow(
+               column(4,
+                      selectInput("mstat",label = "Choose statistical areas to compare",choices = data$STAT_AREA %>% unique %>% sort,multiple = TRUE)
+               ),
+               plotOutput('monthly_plot')
+             )
+    )))
 
 
+server <- shinyServer(function(input, output) {
+ 
+  rxData_daily <- reactive({
+    req(input$dstat)
+    dat <- data %>% 
+      filter(STAT_AREA%in%input$dstat)
+    dat
+  })
+  
+  output$daily_plot <- renderPlot({
+    p <- ggplot(rxData_daily() %>% mutate(date=as.POSIXct(date)), aes(x = date, y = sst.mean,color=factor(STAT_AREA)))
+    p <- p + 
+      geom_line() + 
+      theme_bw() + 
+      xlab("Date") + 
+      ylab("Temperature") + 
+      scale_color_viridis(discrete=TRUE,guide=guide_legend("Statistical Area")) + 
+      scale_x_datetime(date_breaks="1 year",date_labels="%Y") + 
+      theme(legend.position="top",
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=14),
+            axis.text = element_text(size=13),
+            axis.title = element_text(size=14))
+    print(p)
+  })
+  
+  rxData_weekly <- reactive({
+    req(input$wstat)
+    dat <- data %>% 
+      filter(STAT_AREA%in%input$wstat) %>% 
+      group_by(cumwk,STAT_AREA) %>% 
+      summarise(sst.mean=mean(sst.mean,na.rm=TRUE),
+                date=min(date),
+                year=year[1])
+    dat
+  })
+  
+  output$weekly_plot <- renderPlot({
+    p <- ggplot(rxData_weekly() %>% mutate(date=as.POSIXct(date)), aes(x = date, y = sst.mean,color=factor(STAT_AREA)))
+    p <- p + 
+      geom_line() + 
+      theme_bw() + 
+      xlab("Date") + 
+      ylab("Temperature") + 
+      scale_color_viridis(discrete=TRUE,guide=guide_legend("Statistical Area")) + 
+      scale_x_datetime(date_breaks="1 year",date_labels="%Y") + 
+      theme(legend.position="top",
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=14),
+            axis.text = element_text(size=13),
+            axis.title = element_text(size=14))
+    print(p)
+  })
+  
+  rxData_monthly<- reactive({
+    req(input$mstat)
+    dat <- data %>% 
+      filter(STAT_AREA%in%input$mstat) %>% 
+      group_by(cummo,STAT_AREA) %>% 
+      summarise(sst.mean=mean(sst.mean,na.rm=TRUE),
+                date=min(date),
+                year=year[1])
+    dat
+  })
+  
+  output$monthly_plot <- renderPlot({
+    p <- ggplot(rxData_monthly() %>% mutate(date=as.POSIXct(date)), aes(x = date, y = sst.mean,color=factor(STAT_AREA)))
+    p <- p + 
+      geom_line() + 
+      theme_bw() + 
+      xlab("Date") + 
+      ylab("Temperature") + 
+      scale_color_viridis(discrete=TRUE,guide=guide_legend("Statistical Area")) + 
+      scale_x_datetime(date_breaks="1 year",date_labels="%Y") + 
+      theme(legend.position="top",
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=14),
+            axis.text = element_text(size=13),
+            axis.title = element_text(size=14))
+    print(p)
+  })
+})
+
+shinyApp(ui, server)
